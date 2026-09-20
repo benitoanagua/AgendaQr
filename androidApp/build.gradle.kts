3cb33e912360479fcf1f638ea02f790c3c6ba9fb
Benito Anagua <benito.anagua@gmail.com>
2026-09-20 15:24:40 -0400

    feat: configure Android Supabase credentials

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,6 +1,12 @@
 plugins { id("agendaqr.android-application") }
 
-android { namespace = "com.agendaqr.android" }
+android {
+    namespace = "com.agendaqr.android"
+    defaultConfig {
+        buildConfigField("String", "SUPABASE_URL", "\"" + "${providers.gradleProperty("SUPABASE_URL").orNull ?: ""}" + "\"")
+        buildConfigField("String", "SUPABASE_PUBLISHABLE_KEY", "\"" + "${providers.gradleProperty("SUPABASE_PUBLISHABLE_KEY").orNull ?: ""}" + "\"")
+    }
+}
 
 dependencies {
     implementation(project(":shared"))
