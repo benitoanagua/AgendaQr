8c4bfaedb05f5357029f4461233c74a8b503c329
Benito Anagua <benito.anagua@gmail.com>
2026-09-20 15:24:33 -0400

    feat: add Supabase data dependencies

--- feature/destinations/data/build.gradle.kts
+++ feature/destinations/data/build.gradle.kts
@@ -8,12 +8,21 @@ android { namespace = "com.agendaqr.destinations.data" }
 kotlin {
     sourceSets {
         commonMain.dependencies {
+            implementation(platform("io.github.jan-tennert.supabase:bom:${libs.versions.supabase.get()}"))
+            implementation("io.github.jan-tennert.supabase:postgrest-kt")
+            implementation("io.github.jan-tennert.supabase:auth-kt")
+            implementation("io.github.jan-tennert.supabase:storage-kt")
             implementation(project(":feature:destinations:domain"))
             implementation(libs.serialization.json)
             implementation(libs.coroutines.core)
         }
         commonTest.dependencies { implementation(libs.coroutines.test) }
-        androidMain.dependencies { }
+        androidMain.dependencies {
+            implementation("io.ktor:ktor-client-android:${libs.versions.ktor.get()}")
+        }
+        iosMain.dependencies {
+            implementation("io.ktor:ktor-client-darwin:${libs.versions.ktor.get()}")
+        }
         iosMain.dependencies { }
     }
 }
