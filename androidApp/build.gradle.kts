d4e284cb65f4e47f80e34e02daddf86b45cda55d
Benito Anagua <benito.anagua@gmail.com>
2026-09-20 15:25:15 -0400

    chore: keep Supabase configuration in data module

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -2,10 +2,6 @@ plugins { id("agendaqr.android-application") }
 
 android {
     namespace = "com.agendaqr.android"
-    defaultConfig {
-        buildConfigField("String", "SUPABASE_URL", "\"" + "${providers.gradleProperty("SUPABASE_URL").orNull ?: ""}" + "\"")
-        buildConfigField("String", "SUPABASE_PUBLISHABLE_KEY", "\"" + "${providers.gradleProperty("SUPABASE_PUBLISHABLE_KEY").orNull ?: ""}" + "\"")
-    }
 }
 
 dependencies {
