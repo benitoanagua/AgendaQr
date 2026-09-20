37593452f785c9583dedf644a881822dcf8926f0
Benito Anagua <benito.anagua@gmail.com>
2026-09-20 15:34:27 -0400

    feat(operations): enable local-first Supabase synchronization

--- feature/destinations/data/src/androidMain/kotlin/com/agendaqr/destinations/data/PlatformOperationRepositories.android.kt
+++ feature/destinations/data/src/androidMain/kotlin/com/agendaqr/destinations/data/PlatformOperationRepositories.android.kt
@@ -3,5 +3,5 @@ package com.agendaqr.destinations.data
 import com.agendaqr.destinations.domain.ComprobanteRepository
 import com.agendaqr.destinations.domain.OperationRepository
 
-actual fun createOperationRepository(): OperationRepository = LocalOperationRepository()
+actual fun createOperationRepository(): OperationRepository = createSyncedOperationRepository()
 actual fun createComprobanteRepository(): ComprobanteRepository = LocalComprobanteRepository()
