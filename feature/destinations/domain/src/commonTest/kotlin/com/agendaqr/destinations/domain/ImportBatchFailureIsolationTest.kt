43b742f583cace5908667ffdbfb6e1061833f107
Benito Anagua <benito.anagua@gmail.com>
2026-09-23 20:26:33 -0400

    test: clean duplicate assertion import

--- feature/destinations/domain/src/commonTest/kotlin/com/agendaqr/destinations/domain/ImportBatchFailureIsolationTest.kt
+++ feature/destinations/domain/src/commonTest/kotlin/com/agendaqr/destinations/domain/ImportBatchFailureIsolationTest.kt
@@ -6,7 +6,6 @@ import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
-import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 
 class ImportBatchFailureIsolationTest {
