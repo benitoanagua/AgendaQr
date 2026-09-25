3b735df885a79c830a399f9d5a40da2b9d2f4ddb
Benito Anagua <benito.anagua@gmail.com>
2026-09-24 21:05:18 -0400

    fix(ui): map legacy semantic slots to Xauxa roles

--- core/ui/src/commonMain/kotlin/com/agendaqr/core/ui/theme/XauxaTokens.kt
+++ core/ui/src/commonMain/kotlin/com/agendaqr/core/ui/theme/XauxaTokens.kt
@@ -210,6 +210,13 @@ object XauxaColor {
     val Danger: Color @Composable get() = LocalXauxaColorScheme.current.danger
     val OnDanger: Color @Composable get() = LocalXauxaColorScheme.current.onDanger
     val DangerBg: Color @Composable get() = LocalXauxaColorScheme.current.dangerBg
+    val Success: Color @Composable get() = LocalXauxaColorScheme.current.secondary
+    val SuccessBg: Color @Composable get() = LocalXauxaColorScheme.current.secondaryContainer
+    val Warning: Color @Composable get() = LocalXauxaColorScheme.current.tertiary
+    val WarningBg: Color @Composable get() = LocalXauxaColorScheme.current.tertiaryContainer
+    val Info: Color @Composable get() = LocalXauxaColorScheme.current.brand
+    val InfoBg: Color @Composable get() = LocalXauxaColorScheme.current.brandContainer
+    val White: Color = XauxaPrimitive.white
     val OnDangerBg: Color @Composable get() = LocalXauxaColorScheme.current.onDangerBg
     val InverseSurface: Color @Composable get() = LocalXauxaColorScheme.current.inverseSurface
     val InverseOnSurface: Color @Composable get() = LocalXauxaColorScheme.current.inverseOnSurface
